caae6cf7d699ec4222b547016eea04d895d747bb
juancarranza <2052891770114@ingenieria.usac.edu.gt>
2020-03-11 16:17:51 -0600

    Using XML: Demo Setter Injection

--- src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -8,10 +8,14 @@ import java.util.List;
 
 public class SpeakerServiceImpl implements SpeakerService {
 
-    private SpeakerRepository repository = new HibernateSpeakerRepositoryImpl();
+    private SpeakerRepository repository;
 
     @Override
     public List<Speaker> findAll(){
         return repository.findAll();
+    }
+
+    public void setSpeakerRepository(SpeakerRepository repository) {
+        this.repository = repository;
     }
 }
