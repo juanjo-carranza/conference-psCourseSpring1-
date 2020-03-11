db738af71133fe42e356857a33d8f2cfda74a20f
juancarranza <2052891770114@ingenieria.usac.edu.gt>
2020-03-11 16:31:41 -0600

    Using XML: Constructor Injection

--- src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -10,6 +10,14 @@ public class SpeakerServiceImpl implements SpeakerService {
 
     private SpeakerRepository repository;
 
+    public SpeakerServiceImpl(){
+
+    }
+
+    public SpeakerServiceImpl(SpeakerRepository repository){
+        this.repository=repository;
+    }
+
     @Override
     public List<Speaker> findAll(){
         return repository.findAll();
